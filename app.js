1d3faede4e4d72ca13fb4f5c33a0736bea02260f
James Bubb <jpbubb82@gmail.com>
2022-10-12 09:52:34 +0100

    feat: update promise chain with catch and finally

--- app.js
+++ app.js
@@ -7,41 +7,54 @@ const downloadProgressElem = document.querySelector(
 const startDownloadElem = document.getElementById('startDownload');
 
 startDownloadElem.addEventListener('click', () => {
-  fetch(`/${fileToDownload}`).then(response => {
-    const reader = response.body.getReader();
-    let totalDownloaded = 0;
-    const dataChunks = [];
-    const totalSize = Number(response.headers.get('content-length'));
+  startDownloadElem.setAttribute('disabled', 'true');
+  const dataChunks = [];
+  fetch(`/${fileToDownload}`)
+    .then(response => {
+      const reader = response.body.getReader();
+      let totalDownloaded = 0;
+      const totalSize = Number(
+        response.headers.get('content-length')
+      );
 
-    function readData() {
-      reader.read().then(result => {
-        if (result.value) {
-          dataChunks.push(result.value);
-          totalDownloaded += result.value.length;
-          const percentage = Math.floor(
-            (totalDownloaded / totalSize) * 100
-          );
-          console.log(
-            `${totalDownloaded}/${totalSize} (${percentage}%)`
-          );
+      function readData() {
+        return reader.read().then(result => {
+          if (result.value) {
+            dataChunks.push(result.value);
+            totalDownloaded += result.value.length;
+            const percentage = Math.floor(
+              (totalDownloaded / totalSize) * 100
+            );
+            console.log(
+              `${totalDownloaded}/${totalSize} (${percentage}%)`
+            );
 
-          downloadProgressElem.textContent = `${percentage}%`;
-          downloadProgressElem.style.width = `${percentage}%`;
-        }
+            downloadProgressElem.textContent = `${percentage}%`;
+            downloadProgressElem.style.width = `${percentage}%`;
+          }
 
-        if (!result.done) {
-          readData();
-        } else {
-          createAndTriggerDownloadLink(
-            new Blob(dataChunks),
-            fileToDownload
-          );
-        }
-      });
-    }
+          if (!result.done) {
+            return readData();
+          }
+        });
+      }
 
-    readData();
-  });
+      return readData();
+    })
+    .then(() => {
+      createAndTriggerDownloadLink(
+        new Blob(dataChunks),
+        fileToDownload
+      );
+    })
+    .catch(error => {
+      downloadProgressElem.classList.add('error');
+      downloadProgressElem.textContent = 'Download Error';
+      downloadProgressElem.style.width = '100%';
+    })
+    .finally(() => {
+      startDownloadElem.removeAttribute('disabled');
+    });
 });
 
 function createAndTriggerDownloadLink(blob, fileName) {
